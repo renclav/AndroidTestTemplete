8e7efefd3150ae9a066644f0b6722b3ba7a3b2ed
Dylan Green <dylan.green@theculturetrip.com>
2022-03-13 19:28:23 +0000

    added viewbinding linked viewmodel to view

--- app/src/main/java/com/renclav/samplecalendarbooking/DemoApplication.kt
+++ app/src/main/java/com/renclav/samplecalendarbooking/DemoApplication.kt
@@ -1,7 +1,13 @@
 package com.renclav.samplecalendarbooking
 
 import android.app.Application
+import com.airbnb.mvrx.Mavericks
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DemoApplication : Application()
+class DemoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Mavericks.initialize(this)
+    }
+}
--- app/src/main/java/com/renclav/samplecalendarbooking/presentation/BookingSelectionFragment.kt
+++ app/src/main/java/com/renclav/samplecalendarbooking/presentation/BookingSelectionFragment.kt
@@ -1,28 +1,38 @@
 package com.renclav.samplecalendarbooking.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.renclav.samplecalendarbooking.R
+import androidx.fragment.app.Fragment
+import com.airbnb.mvrx.MavericksView
+import com.airbnb.mvrx.fragmentViewModel
+import com.airbnb.mvrx.withState
+import com.renclav.samplecalendarbooking.databinding.BookingSelectionFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-internal class BookingSelectionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+internal class BookingSelectionFragment : Fragment(), MavericksView {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: BookingSelectionFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: BookingSelectionViewModel by fragmentViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.booking_selection_fragment, container, false)
+    ): View {
+        _binding = BookingSelectionFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun invalidate(): Unit = withState(viewModel) { state ->
+        binding.testview.text = state.dummy
     }
 }
